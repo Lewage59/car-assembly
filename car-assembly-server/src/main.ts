e1660e7ac31a0b85ecfd6806873340c1e5e43f1a
Lewage59 <920576696@qq.com>
2021-01-10 10:25:59 +0800

    fix: 处理项目搭建报错问题

--- car-assembly-server/src/main.ts
+++ car-assembly-server/src/main.ts
@@ -4,13 +4,13 @@ import { AppModule } from './app.module';
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
 
-  // 热重载
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+    // 热重载
+    if (module.hot) {
+        module.hot.accept();
+        module.hot.dispose(() => app.close());
+    }
 }
 bootstrap();
