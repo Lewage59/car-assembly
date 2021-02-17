b8d257a95c385e92a3c3d2e03a347fc3bcbe0aca
Lewage59 <920576696@qq.com>
2021-02-17 20:01:34 +0800

    feat: 开发后台管理首页

---
+++ car-assembly-admin/src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '汽车组装后台管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
